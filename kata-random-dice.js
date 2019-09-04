5326f5d39b3bea7f47a6703eba0555da60b63205
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-09-04 12:05:08 -0400

    Delete kata-random-dice.js

--- kata-random-dice.js
+++
@@ -1,8 +0,0 @@
-/*
- * Generate a random integer between 1 and 6 and display as a Window alert and log it to the console.
- */
- // Generate random integer
- var dice = Math.floor(Math.random()*6)+1;
- alert(dice);
- console.log(dice);
- 
