d42b7e8f1b909dc3e94f2956ecea9dde9645a363
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-09-05 11:02:08 -0400

    Update icecream.js

--- ice-cream/icecream.js
+++ ice-cream/icecream.js
@@ -1,21 +1,11 @@
-/* 
- * ICE CREAM KATA
- * This is a kata we will build on.  At level one, it requires knowledge of: 
- * Alerts: https://www.w3schools.com/jsref/met_win_alert.asp
- * Prompts: https://www.w3schools.com/jsref/met_win_prompt.asp
- * At level two, it requires knowledge of conditionals: 
- * https://www.w3schools.com/js/js_if_else.asp
+/* JavaScript Prompts 
+ * Part 1: Specify the container variable
+ * Part 2: Use the right commands
+ * Part 3: Word the prompt
  */
-
-// Level 1: Prompt for a flavor "What flavor scoop?", then show an alert with "Here's your [flavor] scoop. Enjoy!"
-
-// Level 1: Ask "How many scoops?" and show an alert with "Here's your cone with [scoops] scoops of [flavor]. Enjoy!"
-
-// Level 2: Use conditional logic to check responses where flavor is not equal to chocolate or vanilla:
-
-// 	  If strawberry, alert "Sorry, we're all out of strawberry."
-
-// 	  Else alert "Sorry, we don't carry [flavor]."
-
-// For fun, add other conditionals and responses. 
-
+// Declare string variable flavor, set to vanilla as default.
+var flavor = "vanilla";
+// Prompt user for flavor of ice cream cone.
+flavor = prompt("What flavor ice cream?");
+// Offer the ice cream cone, specifying the flavor in the alert message.
+alert("Here is your "+flavor+" ice cream cone.");
