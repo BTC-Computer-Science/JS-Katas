bfe18964f2f6c774805e1f145e863aeddf993ef0
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-09-09 12:00:17 -0400

    Create 1.1-cheese-shop-loop.js

---
+++ cheese-shop/1.1-cheese-shop-loop.js
@@ -0,0 +1,19 @@
+/* JavaScript While Loop
+ * Format: while (boolean expression) {} -- indent block code 
+ */
+ 
+// Declare string cheese flavor, set to Null as default.
+var cheeseType = null;\
+// Define while loop that runs while cheeseType is not equal to stilton
+while (cheeseType != "stilton") {
+  // Prompt user for kind of cheese.
+  cheeseType = prompt("What cheese would you like?");
+  /* Add three conditionals that change based on cheeseType values: if / else if / else */
+  // First one, don't have it. 
+  if (cheeseType=="cheddar") alert("We don't get much call for that around here.");
+  // Middle one being "Here's your [cheeseType] cheese" and 
+  else if (cheeseType=="stilton") alert("Here is your " +cheeseType+ " cheese, sir.");
+  // The last one "Sorry, no."
+  else alert("Sorry, no.");
+  // Close While Loop Brackets
+}
