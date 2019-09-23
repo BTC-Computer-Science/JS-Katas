a2c6df21473c45e5333683e778f149eca01334e9
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-09-23 10:18:34 -0400

    Comments Updated

--- guess/1.0-cheese-to-guess.js
+++ guess/1.0-cheese-to-guess.js
@@ -1,21 +1,23 @@
 /* JavaScript While Loop
  * Format: while (boolean expression) {} -- indent block code 
  */
- 
-// Declare string cheeseType, set to null as default.
+
+// Declare var answer, create as random integer between 1 and 100.
 var answer = Math.floor(Math.random()*100)+1;
+// Console log the answer while testing.
 console.log(answer);
+// Declar var guess, set to 0.
 var guess = 0;
-// Define while loop that runs while cheeseType is not equal to stilton
+// Define while loop that runs while guess is not equal to answer
 while (guess != answer){
-  // Prompt user for guess.
+  	// Prompt user for guess.
 	guess=prompt("Guess my number (1-100)");
-  /* Add three conditionals that change based on cheeseType values: if / else if / else */
-  // First one, don't have it. 
+  	/* Add three conditionals that change based on guess values: if / else if / else */
+  	// First one, too low. 
 	if(guess<answer) alert("too low");
-  // Middle one being "Here's your [cheeseType] cheese" and 
+  	// Middle one, too high. 
 	else if(guess>answer) alert("too high");
-  // The last one "Sorry, no."
+  	// The last one, you got it. 
 	else alert("You got it!");
   // Close While Loop Brackets
 }
