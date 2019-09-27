da8d2cb78036e63318a96159d685f43f7fb38e48
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-09-26 21:01:07 -0400

    Create 1.3-guess-quitter.js

---
+++ guess/1.3-guess-quitter.js
@@ -0,0 +1,27 @@
+/* Guess Game: Random Integer-Driven JavaScript While Loop
+ * Format: while (boolean expression) {} -- indent block code 
+ */
+
+// Declare var answer as random integer between 1 and 100.
+var answer = Math.floor(Math.random()*100)+1;
+// Alert the answer while testing.
+alert(answer);
+// Declare var guess, set to 0.
+var guess = 0;
+// Declare var tries, set to 0.
+var tries = 0;
+// Define while loop that runs while guess is not equal to answer
+while (guess != answer && guess != "q"){
+  // Prompt user for guess.
+	guess=prompt("Guess my number (1-100)");
+	tries++;
+  /* Add feedback based on guess values: if / else if  */
+  // If guess lower than answer, say too low. 
+	if(guess<answer) alert("too low");
+  // If guess greater than answer, say too high. 
+	else if(guess>answer) alert("too high");
+  // Close While Loop Brackets
+}
+// Tell them they got it in however many tries. 
+if (guess == answer) alert("You got it in "+tries+" tries.");
+else alert("Quitter!");
