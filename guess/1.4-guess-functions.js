1c89863fb482fa996073be6ab97e5ccedba2c0a9
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-09-30 10:45:35 -0400

    Update 1.4-guess-functions.js

--- guess/1.4-guess-functions.js
+++ guess/1.4-guess-functions.js
@@ -1,10 +1,12 @@
-/* Adding validator function */
+/* guess merit badge by hannah */
+// guessing game code just like you practiced girl you got this
+// remember: variables. prompt the guesser for the answer and alert them. while, if, else if, else. first one too low, second one too high, q to quit as a way to get out of the loop quick, and last one got it. dont forget to keep track of the answer somewhere, and finish every line with a "bow" ;) lets do this :D
 
 var answer=Math.floor(Math.random()*100)+1;
 var guess=0;
 var tries=0;
 console.log(answer);
-while(guess!=answer&&guess!=-1){
+while(guess!=answer && guess!=-1){
 	guess = guessCheck();
 	tries++;
 	if(guess<answer){
@@ -29,6 +31,7 @@ while(guess!=answer&&guess!=-1){
 */
 function guessCheck() {
 		var goodGuess = false;
+		var guess = 0;
 		while (goodGuess == false) { 
 			guess = prompt("Enter 1-100 or q to quit");
 			if (guess == "q") {
@@ -37,6 +40,7 @@ function guessCheck() {
 				guess = -1;
 			}
 			else if (guess>0 && guess<101) {
+				goodGuess = true;
 			}
 			else {
 				alert("Invalid input. Try again.");
@@ -44,9 +48,4 @@ function guessCheck() {
 		}
 		return guess;
 }
-
-
-
-
-
 
