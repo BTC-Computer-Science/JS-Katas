eb40b9a2d9b7177fcdd57168fd4fb6a6cb74a1b7
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-10-01 13:30:43 -0400

    Update 1.1b-cheese-shop-alt-loop.js

--- cheese-shop/1.1b-cheese-shop-alt-loop.js
+++ cheese-shop/1.1b-cheese-shop-alt-loop.js
@@ -2,19 +2,20 @@
  * Format: while (boolean expression) {} -- indent block code 
  */
  
-// Declare string cheeseType, set to null as default.
+// Declare string variable cheeseType, set to null (no value).
 var cheeseType = null;
-// Define while loop that runs while cheeseType is not equal to stilton
-while (cheeseType != "stilton") {
+var inStock = "stilton";
+// Define while loop that runs while cheeseType is not equal to inStock value
+while (cheeseType != inStock) {
   // Prompt user for kind of cheese.
   cheeseType = prompt("What cheese would you like?");
   /* Add three conditionals that change based on cheeseType values: if / else if / else */
-  // First one, don't have it. 
+  // First one, don't have cheddar because "We don't get much call for that around here"
   if (cheeseType=="cheddar") alert("We don't get much call for that around here.");
-  // We need to specify a response to the condition we want - even if it's do nothing
-  else if (cheeseType=="stilton") {  } //do nothing
-  // The last one "Sorry, no."
+  // Middle one, DO have it: "Here's your [cheeseType] cheese"
+  else if (cheeseType==inStock) {};
+  // Last one, to all other requests: "Sorry, no."
   else alert("Sorry, no.");
   // Close While Loop Brackets
 }
- alert("Here is your " +cheeseType+ " cheese, sir.");
+alert("Here is your " +cheeseType+ " cheese, sir.");
