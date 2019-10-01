84a21e61f198fb571e7e1ecfb13991c9f5eb8e14
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-10-01 15:10:19 -0400

    Update 1.1-cheese-shop-loop.js

--- cheese-shop/1.1-cheese-shop-loop.js
+++ cheese-shop/1.1-cheese-shop-loop.js
@@ -4,6 +4,7 @@
  
 // Declare string variable cheeseType, set to null (no value).
 var cheeseType = null;
+// Declare string variable inStock, set to whatever cheese you have today (like, "stilton")
 var inStock = "stilton";
 // Define while loop that runs while cheeseType is not equal to inStock value
 while (cheeseType != inStock) {
@@ -12,7 +13,7 @@ while (cheeseType != inStock) {
   /* Add three conditionals that change based on cheeseType values: if / else if / else */
   // First one, don't have cheddar because "We don't get much call for that around here"
   if (cheeseType=="cheddar") alert("We don't get much call for that around here.");
-  // Middle one, DO have it: "Here's your [cheeseType] cheese"
+  // Middle one, DO have whatever is in inStock: "Here's your [cheeseType] cheese"
   else if (cheeseType==inStock) alert("Here is your " +cheeseType+ " cheese, sir.");
   // Last one, to all other requests: "Sorry, no."
   else alert("Sorry, no.");
