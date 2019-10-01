f50872de547d69d20cc300cdda6f1ccd89449c71
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-10-01 13:36:59 -0400

    Update 1.3-cheese-shop-boolean-quit.js

--- cheese-shop/1.3-cheese-shop-boolean-quit.js
+++ cheese-shop/1.3-cheese-shop-boolean-quit.js
@@ -2,22 +2,24 @@
  * Format: while (boolean expression) {} -- indent block code 
  */
  
-// Declare string cheeseType, set to null as default.
+// Declare string variable cheeseType, set to null (no value).
 var cheeseType = null;
-// Create Boolean flag inStock
+var inventory = "stilton";
 var inStock = false;
-// Define while loop that runs while cheeseType is not equal to stilton
+// Define while loop that runs while cheeseType is not equal to inStock value
 while (inStock == false) {
   // Prompt user for kind of cheese.
   cheeseType = prompt("What cheese would you like?");
   /* Add three conditionals that change based on cheeseType values: if / else if / else */
-  // First one, don't have it. 
-  if (cheeseType=="cheddar") alert("We don't get much call for that around here.");
-  // We need to change our Boolean Flag to true if we have the cheese
-  else if (cheeseType=="stilton") inStock=true; 
- // deal with Quit by breaking out of the loop
-  else if (cheeseType=="q") break;
-  // The last one "Sorry, no."
+  // First one, don't have cheddar because "We don't get much call for that around here"
+  if (cheeseType == "q") break;
+  else if (cheeseType=="cheddar") alert("We don't get much call for that around here.");
+  // Middle one, DO have it: "Here's your [cheeseType] cheese"
+  else if (cheeseType==inventory) {
+    alert("Here is your " +cheeseType+ " cheese, sir.");
+    inStock = true;
+  }
+  // Last one, to all other requests: "Sorry, no."
   else alert("Sorry, no.");
   // Close While Loop Brackets
 }
