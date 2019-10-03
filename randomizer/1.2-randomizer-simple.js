595b2314bd63a97aadb061c5ac3f9f5fed9016de
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-10-03 11:04:22 -0400

    Create 1.2-randomizer-simple.js

---
+++ randomizer/1.2-randomizer-simple.js
@@ -0,0 +1,11 @@
+/* 
+ * Randomizer: 
+ * Generate a random integer between 1 and 100 
+ * Alert it to the screen
+ * Log it to the console
+ */
+ 
+// var creates a global variable called randomizer equal to 0
+var randomint = Math.floor(Math.random()*100)+1;
+// alert creates a screen pop-up
+alert(randomint);
