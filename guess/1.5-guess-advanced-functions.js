b0305ecfd25fd475f0300d1e64209a10eaf9e0fb
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-10-14 12:07:24 -0400

    Update 1.5-guess-advanced-functions.js

--- guess/1.5-guess-advanced-functions.js
+++ guess/1.5-guess-advanced-functions.js
@@ -1,80 +1,79 @@
-/* Guess Game: Random Integer-Driven JavaScript While Loop
- * Format: while (boolean expression) {} -- indent block code 
- */
-// Declare var playAgain = true
-var playAgain = true;
-// Keep playing new games as long as playAgain equals true.
-while (playAgain == true) {
-	// Declare var answer as random integer between 1 and 100.
-	var answer = Math.floor(Math.random()*100)+1;
-	// alert the answer while testing.
-	alert(answer);
-	// Declare var guess, set to 0.
-	var guess = 0;
-	// NEW: Add turns, games, averageTurns as integers
-	// Define while loop that runs while guess is not equal to answer
-	while (guess != answer){
-		// Prompt user for guess.
-		guess=prompt("Guess my number (1-100)");
-		// break loop if user entered "q" to quit
-		if (guess=="q") break;
-		// proceed if guess is valid (guessValidator returns true)
-		if (guessValidator(guess) == true) { 
-			/* Add conditionals that give feedback on values:  if / else if */
-			// run guessValidator(prompt) and return true if valid guess
-			// If guess less than answer, say too low. 
-			if(guess<answer) alert("too low");
-			// If guess more than answer, say too high. 
-			else if(guess>answer) alert("too high");
-		    // Close While Loop Brackets
-			// Add 1 to turns
-		}
-		// otherwise, guess is not true, so tell them so. 
-		else alert("Invalid guess.  Try again.");
-	}
-	// Go through end game sequence if guess equals answer 
-	if (guess == answer) {
-		// run gameStats on tries
-		gameStats(tries);
-		// run newGame to offer to play again
-		playAgain = newGame();
-	}
-	// otherwise, alert "Quitter!";
-	else alert("Quitter!");
-	// break out of the newGame loop
-	break;
-}	
+/* Guess Validate Function by Mr. M. */
 
-/* Guess Validator 
- * Makes sure it's a good guess so turn can be counted. 
- * Check if Guess is valid integer. If so, return true. 
- * If Guess is invalid, return false. 
+/* Initialize Globals */
+var games = 0;
+var again = true;
+var totalTurns = 0;
+/* Play Again Loop */
+while (again == true) {
+	games++;
+	/* Initialize Locals */
+	var turns = 0;
+	var guess = 0;
+	var answer = Math.floor(Math.random()*100)+1;
+	// console.log(answer);
+	while (guess != answer){
+		guess = prompt("Guess my number (1-100).");
+		if (guess == "q"){
+			alert("You quit.");
+			break;
+		}
+		if (validate(guess) == true){
+			turns++;
+			if (guess < answer){
+				alert("Too low!");
+			}
+			else if (guess > answer){
+				alert("Too high!");
+			}
+			else if (guess == answer) {
+				// call gameStats with turns
+				gameStats(turns);
+				// set again equal to newGame
+				again = newGame();
+			}
+		}
+		else alert("Invalid guess, try again.");
+	}
+}
+// game is over
+
+/* Function validate
+ * Ensures guess is a valid integer within answer range
  * @param guess
  * @return boolean
  */
-function guessValidator(guess){
-	
+function validate(guess){
+	if (guess > 0 && guess < 101){
+		return true;
+	}
+	else return false;
 }
 
-/* Game Stats
- * Give player feedback on performance
- * Display turns
- * Add turns to totalTurns
- * Calculate average turns = totalTurns / games
- * Display average turns
- * @return totalturns
+/* Function gameStats
+ * shows player games and average turns per game calculated as totalTurns / games
+ * @param none
+ * @return none
  */
-function gameStats(){
-    	
+function gameStats(turns){
+	alert("You got it in "+turns+" turns!");
+	totalTurns += turns;
+	var averageTurns = totalTurns / games;
+	alert("You won "+games+" games with an average of "+averageTurns+" turns per game!");
 }
-
-/* Play Again
- * Provide option to stop playing without quitting
- * Prompt for Y or N
- * If Y return true
- * If N alert "Thank you for playing" and return false
- * @return boolean
+	
+/* Function newGame
+ * Gives player option to play again (y) and returns boolean again, y = true
+ * @param none
+ * @return again 
  */
 function newGame(){
-	
+	var again = prompt("Play again? y = yes.");
+	if (again == "y") {
+		return true;
+	}
+	else {
+		alert("Sorry to see you go!");
+		return false;
+	}
 }
