461002cc0e535a7602223add28ca7af245dc19b4
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-10-14 20:02:43 -0400

    Update 1.4-guess-validator-function.js

--- guess/1.4-guess-validator-function.js
+++ guess/1.4-guess-validator-function.js
@@ -17,7 +17,7 @@ while (guess != answer){
 	// break out of loop if guess equals q. 
         if (guess =="q") break;
 	// if guessValidator returns true
-	if (guessValidator(guess)==true){
+	if (validator(guess)==true){
 		// Add 1 to tries
 		tries++; 
 		// If guess lower than answer, say too low. 
@@ -35,14 +35,14 @@ if (guess == answer) alert("You got it in "+tries+" tries.");
 // Otherwise call them a quitter
 else alert("Quitter!");
 
-/* Function guessValidator
+/* Function validator
  * Checks if guess is valid integer within range (1-100 default)
  * If guess not valid, return false;
  * If guess valid, return true
  * @param guess
  * @return boolean 
  */
-function guessValidator(guess){
+function validator(guess){
 	// return true if guess is a valid integer within range 
 	if (guess > 0 && guess < 101) return true;
 	// otherwise, guess is not valid. Return false. 
