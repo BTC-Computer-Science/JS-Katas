a59f7ce9dd7aeb453e0fd9f2ba326edcc910f942
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-10-16 13:40:37 -0400

    Update 1.3-random-dice.js

--- randomizer/1.3-random-dice.js
+++ randomizer/1.3-random-dice.js
@@ -1,26 +1,24 @@
 /* Dice Kata */
 
 /* Global variables */
-set variable dieSides equal to 6 as default
-set variable dice equal to 2 as default
-set variable lastDie equal to 0;
-set variable rollSum equal to 0;
-
+var dieSides = 6;
+var diceNumber = 2;
+var totalRolled = 0;
+var rollSum = 0;
 // Ask user for how many dieSides (D&D dice go from 4 to 20)
-
+dieSides=prompt("How many sides do you want on your dice?");
 // Ask the user for how many dice to roll
-
+diceNumber=prompt("How many dice do you want to roll?");
 /* Use a while loop to keep rolling until you have rolled all the dice, adding them together */
-
-// While lastDie is not equal to dice, keep rolling
-
-// Add 1 to lastDie
-
-// Add a roll to rollSum
-
+// While totalRolled is less than diceNumber, keep rolling
+while (totalRolled < diceNumber) {
+// roll the die
+   roll = Math.floor(Math.random()*dieSides)+1;
+// Add new roll to rollSum
+   rollSum += roll; 
+// Add 1 to totalRolled
+  totalRolled ++;
 // End Loop
-
+}
 // Alert the roll to the user
-
-
-
+alert("You rolled a " + rollSum);
