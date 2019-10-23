e8a3eebbde465e4618b9431f865096975250d341
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-10-23 11:57:04 -0400

    Create 1.4k-guess-validate-kata.js

---
+++ guess/1.4k-guess-validate-kata.js
@@ -0,0 +1,49 @@
+/* Guess Validator Function: Merit badge 2
+ * Adds counting turns, quit with q, and validating guess
+ * By Mr. M. 
+ */
+
+// Declare var answer as random integer between 1 and 100.
+var answer = Math.floor(Math.random()*100)+1;
+// Alert the answer while testing.
+console.log(answer);
+// Declare var guess, set to 0.
+var guess = 0;
+// Declare var turns, set to 0.
+
+// Define while loop that runs while guess is not equal to answer
+while (guess != answer){
+  	// Prompt user for guess.
+	guess=prompt("Guess my number (1-100)");
+	// break out of loop if guess equals q. 
+
+	// if guessValidator returns true
+	if (validator(guess)==true){
+		// Add 1 to turns
+
+		// If guess lower than answer, say too low. 
+		if (guess<answer) alert("too low");
+		// Otherwise, if guess greater than answer, say too high. 
+		else if (guess>answer) alert("too high");
+	// Close If block
+	}
+	// alert "Invalid Guess.  Try again!" if guessValidator does not return true.
+
+	// Close While Loop block
+}
+// If they guessed it, tell them they got it in however many turns. 
+if (guess == answer) alert("You got it in "+turns+" turns.");
+// Otherwise call them a quitter
+
+
+/* Function validator
+ * Checks if guess is valid integer within range (1-100 default)
+ * If guess not valid, return false;
+ * If guess valid, return true
+ * @param guess
+ * @return boolean 
+ */
+
+	// return true if guess is a valid integer within range 
+	// otherwise, guess is not valid. Return false. 
+
