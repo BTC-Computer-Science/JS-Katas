57881dc657db569ec21ece40ccafd8a1d708d70e
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-10-28 12:18:22 -0400

    Update 1.6-guess-advanced-sr.js

--- guess/1.6-guess-advanced-sr.js
+++ guess/1.6-guess-advanced-sr.js
@@ -1,49 +1,43 @@
 /* Guess Game: Random Integer-Driven JavaScript While Loop */
 // Declare var playAgain = true
 var playAgain = true;
+// Declare totalTurns = 0;
 var totalTurns = 0;
+// Declare games = 0;
 var games = 0;
-
+// set a while loop as long as playAgain is true
 while (playAgain == true) {
-  var turns = 0;
-  
-	// Declare var answer as random integer between 1 and 100.
-	var answer = Math.floor(Math.random()*100)+1;
-	// alert the answer while testing.
-	alert(answer);
 	// Declare var guess, set to 0.
 	var guess = 0;
-	// NEW: Add turns, games, averageTurns as integers
+	// re=set turns since new game
+  	var turns = 0;
+	// Declare var answer as random integer between 1 and 100.
+	var answer = Math.floor(Math.random()*100)+1;
+	// log  the answer while testing.
+	console.log(answer);
 	// Define while loop that runs while guess is not equal to answer
 	while (guess != answer && guess != "q"){
 		// Prompt user for guess.
 		guess=prompt("Guess my number (1-100)");
-		if (guessValidator == true) { 
+		if (validator(guess) == true) { 
 			/* Add conditionals that give feedback on values:  if / else if */
 			// run guessValidator(prompt) and return true if valid guess
 			// If guess less than answer, say too low. 
 			if(guess<answer) alert("too low");
 			// If guess more than answer, say too high. 
 			else if(guess>answer) alert("too high");
-		    // Close While Loop Brackets
 			// Add 1 to turns
+			turns++;
+		        // Close While Loop Brackets	
 		}
-    turns++;
-    
-    if (guess!="q" && guess!=answer) {
-      alert("Invalid guess.  Try again.");
-    }
-	}
-  
-  if (guess=="q") {
-    alert("Quitter!!");
-  }
-  if (guess!="q") {
-    gameStats();
-    games++;
-  }
-  
-	playAgain = newGame();
+      		else alert("Invalid guess.  Try again.");
+    	}
+  	if (guess=="q") alert("Quitter!!");
+	else if (guess==answer) {
+	    	gameStats();
+	    	games++;
+		playAgain = newGame();
+  	}
 }	
 
 /* Guess Validator 
@@ -54,7 +48,7 @@ while (playAgain == true) {
  * @param guess
  * @return boolean
  */
-function guessValidator(guess){
+function validator(guess){
   if (guess > 0 && guess < 101) {
     return(true);
   } else {
