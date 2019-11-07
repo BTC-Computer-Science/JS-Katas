772dd941bc8197f7044e3964ae2b5e219f92734b
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-11-07 11:53:27 -0500

    Update 1-how-many-boxes.js

--- boxes/1-how-many-boxes.js
+++ boxes/1-how-many-boxes.js
@@ -1,9 +1,7 @@
 // Ask users for how many boxes of Rose-Colored Glasses they want
-var rcgBoxes = prompt("How many boxes of Rose-Colored Glasses do you want?");
+
 // Ask users for how many boxes of Pink Paint they want
-var ppBoxes = prompt("How many boxes of Pink Paint do you want?");
-// Compare the number of boxes of each -- which is more or less? Alert a sentence comparing them. 
-if (ppBoxes > rcgBoxes) alert (ppBoxes+" paint boxes is more than "+rcgBoxes+" glasses boxes.");
-else alert (ppBoxes+"ppBoxes is less than "+rcgBoxes+" rcgBoxes.");
+
+// Alert which number of boxes is higher, e.g. "You want more Paints than Glasses"
+
 // Alert the total number of boxes to be delivered
-alert("Expect to receive "+(Number(rcgBoxes) + Number(ppBoxes))+" boxes.");
