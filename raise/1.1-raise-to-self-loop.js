c138597156a3fe448bece70e8b6e3dcb167f622a
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-12-24 10:51:24 -0500

    Update 1.1-raise-to-self-loop.js

--- raise/1.1-raise-to-self-loop.js
+++ raise/1.1-raise-to-self-loop.js
@@ -4,24 +4,24 @@
 /* Global Section */
 
 /* Initializations */ 
-// Set integer intInput to value of prompt "Integer to raise to itself"
-var intInput = prompt("Which number would tyou like to elevate?");
+// Set integer intInput to value of prompt "Integer to raise to itself" parsed as an Integer.
+var input = parseInt(prompt("Which number would you like to elevate?"));
 
 /* Procedures */
-// in an alert, call raiseToSelf function referencing global intInput
-alert(raiseToSelf(intInput));
+// in an alert, call raiseToSelf function referencing global input
+alert(raiseToSelf(input));
 
 /* Function Definitions Section */
 
 // Define raiseToSelf Function 
-function raiseToSelf() {
-  // define local variable intPower equal to intInput	
-  var intPower=intInput;
-  // create a for loop to do each multiplication: Start at 1, Stop before i = intInput, step by 1
-  for(i=1;i<intInput;i++) {
-  // multiply intPower by intInput every time you run the loop
-    intPower = intPower * intInput;
-  }
+function raiseToSelf(input) {
+	// define local variable called result, set it equal to intInput	
+	var result=input;
+	// create a for loop to do each multiplication: Start at 1, Stop before i = intInput, step by 1
+	for(i=1;i<input;i++) {
+		// multiply intPower by intInput every time you run the loop
+		result = result * input;
+  	}
 	// return intPower 
-  return intPower;
+ 	 return result;
 }
