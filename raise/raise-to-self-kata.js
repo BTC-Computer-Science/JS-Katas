d549bb7663bc01555ddeede577c1a2f28b64005d
bmoreini <42300498+bmoreini@users.noreply.github.com>
2020-01-06 12:19:27 -0500

    Create raise-to-self-kata.js

---
+++ raise/raise-to-self-kata.js
@@ -0,0 +1,21 @@
+/* Function to raise a number to itself using a loop */
+
+// Set integer intInput to value of prompt "Integer to raise to itself" parsed as an Integer.
+var input = parseInt(prompt("Which number would you like to elevate?"));
+// in an alert, call raiseToSelf function referencing global input
+alert(raiseToSelf(input));
+
+/* Function Definitions Section */
+
+// Define raiseToSelf Function -- take input as parameter
+
+	// define local variable called result, set it equal to intInput	
+
+	// create a for loop to do each multiplication: Start at 1, Stop before i = intInput, step by 1
+
+		// multiply intPower by intInput every time you run the loop
+
+  	}
+	// return intPower 
+
+}
